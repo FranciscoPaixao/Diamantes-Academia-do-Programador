df4bc1582ebc0e2c44472f7505375636d27fe205
FranciscoPaixao <26686029+FranciscoPaixao@users.noreply.github.com>
2023-03-15 17:27:30 -0300

    Refinamento do programa

--- Diamantes-Academia-do-Programador/Program.cs
+++ Diamantes-Academia-do-Programador/Program.cs
@@ -19,37 +19,14 @@
                 }
             }
             int meio = (num / 2) + 1;
-            int quantidadeHashtag = 1;
-            int[,] matriz = new int[num, num];
+            int quantidadeHashtag = 0;
             for (int i = 0; i < meio; i++)
             {
-                for (int j = 0; j < num; j++)
-                {
-                    if (j >= meio - quantidadeHashtag && j < meio + quantidadeHashtag - 1)
-                    {
-                        matriz[i, j] = 1;
-                    }
-                }
                 quantidadeHashtag++;
-            }
-            quantidadeHashtag = meio;
-            for (int i = meio; i < num; i++)
-            {
-                quantidadeHashtag--;
+                Console.WriteLine("");
                 for (int j = 0; j < num; j++)
                 {
                     if (j >= meio - quantidadeHashtag && j < meio + quantidadeHashtag - 1)
-                    {
-                        matriz[i, j] = 1;
-                    }
-                }
-            }
-
-            for (int i = 0; i < num; i++)
-            {
-                for (int j = 0; j < num; j++)
-                {
-                    if (matriz[i, j] == 1)
                     {
                         Console.Write("#");
                     }
@@ -58,7 +35,23 @@
                         Console.Write(" ");
                     }
                 }
+            }
+            quantidadeHashtag = meio;
+            for (int i = meio; i < num; i++)
+            {
+                quantidadeHashtag--;
                 Console.WriteLine("");
+                for (int j = 0; j < num; j++)
+                {
+                    if (j >= meio - quantidadeHashtag && j < meio + quantidadeHashtag - 1)
+                    {
+                        Console.Write("#");
+                    }
+                    else
+                    {
+                        Console.Write(" ");
+                    }
+                }
             }
             while (true) ;
         }
