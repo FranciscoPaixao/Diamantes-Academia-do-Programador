90431c81ffbaaa672d53f77178c5f16375123db0
FranciscoPaixao <26686029+FranciscoPaixao@users.noreply.github.com>
2023-03-15 15:47:36 -0300

    Add project files.

---
+++ Diamantes-Academia-do-Programador/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Diamantes_Academia_do_Programador
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
