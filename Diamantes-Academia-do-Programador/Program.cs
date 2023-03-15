313fb2c763a639025e4164bb4e04348a1b5dcdbf
FranciscoPaixao <26686029+FranciscoPaixao@users.noreply.github.com>
2023-03-15 16:53:33 -0300

    Fim

--- Diamantes-Academia-do-Programador/Program.cs
+++ Diamantes-Academia-do-Programador/Program.cs
@@ -4,7 +4,63 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int num;
+            while (true)
+            {
+                Console.Write("Informe um numero impar: ");
+                num = Convert.ToInt32(Console.ReadLine());
+                if (num % 2 == 0)
+                {
+                    Console.WriteLine("O numero nao e impar!");
+                }
+                else
+                {
+                    break;
+                }
+            }
+            int meio = (num / 2)+1;
+            int quantidadeHashtag = 1;
+            int[,] matriz = new int[num, num];
+            for (int i = 0; i < meio; i++)
+            {
+                for (int j = 0; j < num; j++)
+                {
+                    if (j >= meio - quantidadeHashtag && j  < meio + quantidadeHashtag-1)
+                    {
+                        matriz[i, j] = 1;
+                    }
+                }
+                quantidadeHashtag++;
+            }
+            quantidadeHashtag = meio;
+            for (int i = meio; i < num; i++)
+            {
+                quantidadeHashtag--;
+                for (int j = 0; j < num; j++)
+                {
+                    if (j >= meio - quantidadeHashtag && j < meio + quantidadeHashtag - 1)
+                    {
+                        matriz[i, j] = 1;
+                    }
+                }
+            }
+
+            for (int i = 0; i < num; i++)
+            {
+                for (int j = 0; j < num; j++) { 
+                    if (matriz[i, j] == 1)
+                    {
+                        Console.Write("#");
+                    }
+                    else
+                    {
+                        Console.Write(" ");
+                    }
+                }
+                Console.WriteLine("");
+            }
+                    while (true);
         }
+
     }
 }
